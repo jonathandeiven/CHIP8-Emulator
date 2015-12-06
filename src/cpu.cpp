01e68bd0b3e6a4e07b690ad8549ac7d6842327a2
Jonathan <jdeiven@outlook.com>
2015-12-06 12:17:22 -0500

    Completed simple op codes

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -62,6 +62,8 @@ void Chip8::cycle() {
 	// Fetch two bytes and merge them to get opcode
 	opcode = memory[pc] << 8 | memory[pc + 1];
 
+	pc += 2;
+
 	// Decode opcode
 	switch(opcode & 0xF000)
 	{
@@ -72,12 +74,10 @@ void Chip8::cycle() {
 					for (int i = 0; i < GFX_SIZE; i++) {
 						gfx[i] = 0;
 					}
-					pc += 2;
 					break;
 
 				case 0x000E: // 0x00EE: Return from subroutine
 					pc = stack[--sp];
-					pc += 2;
 					break; 
 
 				default:
@@ -88,34 +88,57 @@ void Chip8::cycle() {
 
 		case 0x1000: // 0x1NNN: Jump to address NNN
 			pc = opcode & 0x0FFF;
-			pc += 2;
 			break;
 
 		case 0x2000: // 0x2NNN: Calls subroutine at NNN
+			stack[sp++] = pc;
+			pc = opcode & 0x0FFF;
+			break;
 
 		case 0x3000: // 0x3XNN: Skips next instruction if VX = NN
+			if (V[X] == (opcode & 0x00FF))
+				pc += 2;
+			break;
 
 		case 0x4000: // 0x4XNN: Skips next instruction if VX != NN
+			if (V[X] != (opcode & 0x00FF))
+				pc += 2;
+			break;
 
 		case 0x5000: // 0x5XY0: Skips next instruction if VX = VY
+			if (V[X] == V[Y])
+				pc += 2;
+			break;
 
 		case 0x6000: // 0x6XNN: Sets VX = NN
+			V[X] = (opcode & 0x00FF);
+			break;
 
 		case 0x7000: // 0x7XNN: Adds NN to VX
+			V[X] += (opcode & 0x00FF);
+			break;
 
 		case 0x8000:
 			switch(opcode & 0x000F)
 			{
-				case 0x0: // 0x8XY0: Sets VY to value of VY
+				case 0x0: // 0x8XY0: Sets VX to value of VY
+					V[X] = V[Y];
+					break;
 
 				case 0x1: // 0x8XY2: Sets VX to VX or VY
+					V[X] = (V[X] | V[Y]);
+					break;
 
 				case 0x2: // 0x8XY2: Sets VX to VX and VY
+					V[X] = (V[X] & V[Y]);
+					break;
 
 				case 0x3: // 0x8XY3: Sets VX to VX xor VY
+					V[X] = (V[X] ^ V[Y]);
+					break;
 
 				case 0x4: // 0x8XY4: Adds VY to VX; VF is 1 when
-				          // there's a carry and 0 if not
+						  // there's a carry and 0 if not
 
 				case 0x5: // 0x8XY5: VY subtracted from VX. VF is 
 						  // 0 if there's a borrow and 1 if not
@@ -136,15 +159,19 @@ void Chip8::cycle() {
 			break;
 
 		case 0x9000: // 0x9XY0: Skips next instruction if VX != VY
+			if (V[X] != V[Y])
+				pc += 2;
+			break;
 
 		case 0xA000: // 0xANNN: Set I to address NNN
 			I = opcode & 0x0FFF;
-			pc += 2;
 			break;
 
 		case 0xB000: // 0xBNNN: Jumps to address of NNN + V0
+			pc = (opcode & 0x0FFF) + V[0];
+			break;
 
-		case 0xC000: // 0xCXNN: Sets VX to result of bitwise operationg
+		case 0xC000: // 0xCXNN: Sets VX to result of bitwise operation
 					 // on random number and NN
 
 		case 0xD000: // 0xDXYN: Graphics sprite
@@ -168,14 +195,22 @@ void Chip8::cycle() {
 			switch(opcode & 0x00FF)
 			{
 				case 0x07: // 0xFX07: Sets VX to delay timer value
+					V[X] = delay_timer;
+					break;
 
 				case 0x0A: // 0xFX0A: Key press awaited, and stored in VX
 
 				case 0x15: // 0xFX15: Sets delay timer to VX
+					delay_timer = V[X];
+					break;
 
 				case 0x18: // 0xFX18: Sets sound timer to VX
+					sound_timer = V[X];
+					break;
 
 				case 0x1E: // 0xFX1E: Adds VX to I
+					I += V[X];
+					break;
 
 				case 0x29: // 0xFX29: Sets I to location of sprite
 
@@ -184,8 +219,8 @@ void Chip8::cycle() {
 				case 0x55: // 0xFX55: Stores V0 to VX in memory address
 						   // starting at address I
 
-				case 0x65: // Fills V0 to VX with values from memory
-						   // starting at address I
+				case 0x65: // 0xFX65: Fills V0 to VX with values from
+						   // memory starting at address I
 
 				default:
 					printf("Opcode unknown: 0x%X\n", opcode);
