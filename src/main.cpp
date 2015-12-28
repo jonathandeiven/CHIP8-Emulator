05267b44a495a306b949c0ce3eb1c88c111d501c
Jonathan <jdeiven@outlook.com>
2015-12-28 16:51:09 -0500

    Changed build system to CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
+#include <thread>
 #include "cpu.h"
-
-using namespace std;
 
 Chip8 chip8;
 
 int main(int argc, char **argv) {
-
+	
 	if (argc < 2)
 	{
-		cout << "Missing ROM file. Usage: chip8-emulator <ROM>" << endl;
+		std::cout << "Missing ROM file. Usage: chip8-emulator <ROM>" << std::endl;
 		exit (EXIT_FAILURE);
 	}
 
@@ -20,6 +19,7 @@ int main(int argc, char **argv) {
 	{
 		// Emulate one cycle
 		chip8.cycle();
+		std::this_thread::sleep_for(std::chrono::milliseconds(17)); // 60Hz CPU
 
 		// Update screen
 		//if (chip8.drawFlag)
